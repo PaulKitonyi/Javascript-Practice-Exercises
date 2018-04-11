8068ef38c6a45bec3e92439a4509a3738bdc62bc
paulkitonyi <paulmucimah@gmail.com>
2018-04-11 16:19:49 -0700

    Renamed the class

---
+++ User.js
@@ -0,0 +1,32 @@
+"use scrict"
+class User{
+    constructor(username, email, password){
+        this.username = username;
+        this.email    = email;
+        this.password = password;
+    }
+
+    register(){
+        console.log(this.username + ' is now registered.')
+    }
+}
+
+// subclass
+class Member extends User{
+    constructor(username, email, password, memberPackage){
+        super(username, email, password);
+        this.package = memberPackage;
+    }
+
+    getPackage(){
+        console.log(this.username + ' is subscribed to '+ this.package + ' Member Package')
+    }
+}
+
+// for testing purpose!!!!!!
+let paul = new User('Paul kitonyi', 'paulmucimah@gmail.com', 'password')
+paul.register();
+
+// testing sub class!!!!!
+let mike = new Member('Mike', 'mike@gmail.com', 'password', 'standard')
+mike.getPackage();
