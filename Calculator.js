4c2d74ad333c74de7fc1585196839f2f12d11de9
paulkitonyi <paulmucimah@gmail.com>
2018-04-11 17:11:36 -0700

    modified the script

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,28 @@
 "use strict"
+
+class Calculator{
+    add(firstvalue,secondvalue){
+        console.log('The sum of ' + firstvalue + '+' + secondvalue + '=',firstvalue + secondvalue);
+    }
+
+    subtract(firstvalue,secondvalue){
+        console.log('The subtraction of '+ firstvalue + '-' + secondvalue + '=',firstvalue-secondvalue);
+    }
+
+    multiply(firstvalue,secondvalue){
+        console.log('The  multiplication of '+ firstvalue + 'x'+ secondvalue + '=',firstvalue*secondvalue);
+    }
+
+    division(firstvalue,secondvalue){
+        console.log('The division of '+ firstvalue + '/' + secondvalue + '=', firstvalue / secondvalue)
+    }
+}
+
+
+
+// For testing purposes
+const calculator = new Calculator();
+calculator.add(6,5);
+calculator.subtract(7,4);
+calculator.multiply(6,9);
+calculator.division(9,3);
